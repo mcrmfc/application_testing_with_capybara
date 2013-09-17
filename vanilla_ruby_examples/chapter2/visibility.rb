7fd0515ca38d41f1641afe9cf019abd28a48cc01
mcrmfc <matthew.robbins@itv.com>
2013-09-17 17:39:41 +0100

    add unit test examples for matching strategies

--- vanilla_ruby_examples/chapter2/visibility.rb
+++ vanilla_ruby_examples/chapter2/visibility.rb
@@ -1,15 +1,57 @@
-##You can use this example to play around with the different matching strategies
-#in Caybara 2.1
 require 'bundler/setup'
+require 'minitest/autorun'
 require 'capybara'
+dir = File.dirname(__FILE__)
+$LOAD_PATH.unshift "#{dir}/../lib"
 
-page = Capybara::Session.new :selenium
-page.visit('http://capybara.local/html/chapter2/visibility.html')
+class CapybaraMatchingStrategyTest < MiniTest::Unit::TestCase
 
-Capybara.match = :smart
-#Capybara.exact = false
-#session.find('.result')
+  def setup
+    @page = Capybara::Session.new :selenium
+    @page.visit('http://localhost/capybara/html/chapter2/visibility.html')
+  end
 
-page.has_selector? '.result', { :visible => true, :text => 'Match 1' }
-page.fill_in 'complete this', :with => 'foobar'
-page.fill_in 'complete this ', :with => 'bazqux'
+  def test_first_takes_first_match_partial_or_exact
+    Capybara.match = :first
+    Capybara.exact = false
+    @page.has_selector? '.result', { :visible => true, :text => 'Match 1' }
+    @page.fill_in 'please complete this', :with => 'foobar'
+    @page.fill_in 'please complete', :with => 'bazqux'
+    assert_equal 'bazqux', @page.find('#text1').value
+    assert_equal '', @page.find('#text2').value
+  end
+
+  def test_prefer_exact_takes_the_first_exact_then_first_partial_match
+    Capybara.match = :prefer_exact
+    Capybara.exact = false
+    @page.has_selector? '.result', { :visible => true, :text => 'Match 1' }
+    @page.fill_in 'please complete this', :with => 'foobar'
+    @page.fill_in 'please complete', :with => 'bazqux'
+    assert_equal 'foobar', @page.find('#text2').value
+    assert_equal 'bazqux', @page.find('#text1').value
+  end
+
+
+  def test_smart_will_prefer_exact_but_raise_ambiguous_error_for_multiple_matches
+    Capybara.match = :smart
+    Capybara.exact = false
+    @page.has_selector? '.result', { :visible => true, :text => 'Match 1' }
+    @page.fill_in 'please complete this', :with => 'foobar'
+    assert_equal 'foobar', @page.find('#text2').value
+    assert_raises(Capybara::Ambiguous) {@page.fill_in 'please complete', :with => 'bazqux'}
+  end
+
+  def test_one_will_raise_ambiguous_error_for_any_multiple_matches
+    Capybara.match = :one
+    Capybara.exact = false
+    @page.has_selector? '.result', { :visible => true, :text => 'Match 1' }
+    assert_raises(Capybara::Ambiguous) {@page.fill_in 'please complete this', :with => 'foobar'}
+  end
+
+  def test_setting_exact_to_true_will_raise_ambiguous_error_for_any_multiple_matches
+    Capybara.match = :one
+    Capybara.exact = true
+    @page.has_selector? '.result', { :visible => true, :text => 'Match 1' }
+    assert_raises(Capybara::ElementNotFound) {@page.fill_in 'please complete', :with => 'foobar'}
+  end
+end
