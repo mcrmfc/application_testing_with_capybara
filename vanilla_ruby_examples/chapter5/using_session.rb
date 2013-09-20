47f3f53bab850026d56fd84a6b192050c607e8f8
mcrmfc <matthew.robbins@itv.com>
2013-09-20 11:08:09 +0100

    fix minor bug where text not extracted from element correctly

--- vanilla_ruby_examples/chapter5/using_session.rb
+++ vanilla_ruby_examples/chapter5/using_session.rb
@@ -4,4 +4,4 @@ require 'capybara'
 session = Capybara::Session.new :selenium
 session.visit('http://capybara.local/html/chapter4/html5.html')
 session.click_on 'Play'
-raise 'song not playing' unless session.find('#log') == 'playing'
+raise 'song not playing' unless session.find('#log').text == 'playing'
