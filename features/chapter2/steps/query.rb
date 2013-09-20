7181d36cc91ca568c7536ed38c4c7cec96ff49e3
mcrmfc <matthew.robbins@itv.com>
2013-09-20 12:20:01 +0100

    minor fix to steps following markup change

--- features/chapter2/steps/query.rb
+++ features/chapter2/steps/query.rb
@@ -5,8 +5,8 @@ end
 When(/^I search for content using Capybara's query API$/) do
   #For illustration purposes show a number of different options in single step - not best practice ;-)
   page.has_link? 'Google!'
-  page.has_selector? 'div[id=local_results]'
-  page.has_xpath? "//div[@id='local_results']"
+  page.has_selector? 'ul[id=local_results]'
+  page.has_xpath? "//ul[@id='local_results']"
   page.has_no_selector? '#mock_results'
   page.should have_no_selector '#mock_results'
 end
@@ -17,7 +17,7 @@ Then(/^the correct elements are located$/) do
   first('#res1').should have_content 'Local Match 1'
   first('#res1')[:class].should == 'result'
   page.should have_link 'Google!'
-  page.should have_selector 'div[id=local_results]'
-  page.should have_xpath "//div[@id='local_results']"
+  page.should have_selector 'ul[id=local_results]'
+  page.should have_xpath "//ul[@id='local_results']"
   page.should have_no_selector '#mock_results'
 end
